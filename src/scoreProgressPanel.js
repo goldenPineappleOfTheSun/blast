2154ada4ae1cff9bdab3319d28493222c1cfb4cb
saetov <djcore@yandex.ru>
2022-10-03 20:36:54 +0500

    money2 panel

--- src/scoreProgressPanel.js
+++ src/scoreProgressPanel.js
@@ -5,14 +5,6 @@ export class ScoreProgressPanel {
 
     constructor(x, y, width, height) {
         this.#sprite = new PIXI.Container();
-
-        let panelSprite = new PIXI.Graphics();
-        panelSprite.beginFill(0xDE3249);
-        panelSprite.drawRect(0, 0, width, height);
-        panelSprite.endFill();
-        panelSprite.x = x;
-        panelSprite.y = y;
-        this.#sprite.addChild(panelSprite);
     }
 
     getSprite() {
